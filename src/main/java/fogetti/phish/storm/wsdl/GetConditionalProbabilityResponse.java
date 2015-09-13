faad86b4d510bba7b2ddaa44617b4ab6c907dffd
Fogetti <fogetti@gmail.com>
2015-09-14 00:09:36 +0900

    Initial commit: URLSpout started, added URL segmentation, generated Bing service WSDL

---
+++ src/main/java/fogetti/phish/storm/wsdl/GetConditionalProbabilityResponse.java
@@ -0,0 +1,64 @@
+
+package fogetti.phish.storm.wsdl;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="GetConditionalProbabilityResult" type="{http://www.w3.org/2001/XMLSchema}float" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "getConditionalProbabilityResult"
+})
+@XmlRootElement(name = "GetConditionalProbabilityResponse")
+public class GetConditionalProbabilityResponse {
+
+    @XmlElement(name = "GetConditionalProbabilityResult")
+    protected Float getConditionalProbabilityResult;
+
+    /**
+     * Gets the value of the getConditionalProbabilityResult property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Float }
+     *     
+     */
+    public Float getGetConditionalProbabilityResult() {
+        return getConditionalProbabilityResult;
+    }
+
+    /**
+     * Sets the value of the getConditionalProbabilityResult property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Float }
+     *     
+     */
+    public void setGetConditionalProbabilityResult(Float value) {
+        this.getConditionalProbabilityResult = value;
+    }
+
+}
---
+++ src/main/java/fogetti/phish/storm/wsdl/GetProbabilitiesResponse.java
@@ -0,0 +1,65 @@
+
+package fogetti.phish.storm.wsdl;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="GetProbabilitiesResult" type="{http://schemas.microsoft.com/2003/10/Serialization/Arrays}ArrayOffloat" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "getProbabilitiesResult"
+})
+@XmlRootElement(name = "GetProbabilitiesResponse")
+public class GetProbabilitiesResponse {
+
+    @XmlElementRef(name = "GetProbabilitiesResult", namespace = "http://schemas.microsoft.com/research/2011/08/wiab", type = JAXBElement.class, required = false)
+    protected JAXBElement<ArrayOffloat> getProbabilitiesResult;
+
+    /**
+     * Gets the value of the getProbabilitiesResult property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link ArrayOffloat }{@code >}
+     *     
+     */
+    public JAXBElement<ArrayOffloat> getGetProbabilitiesResult() {
+        return getProbabilitiesResult;
+    }
+
+    /**
+     * Sets the value of the getProbabilitiesResult property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link ArrayOffloat }{@code >}
+     *     
+     */
+    public void setGetProbabilitiesResult(JAXBElement<ArrayOffloat> value) {
+        this.getProbabilitiesResult = value;
+    }
+
+}
