8542023769858db8a88f99f4bf5235ce17bc6430
Fogetti <fogetti@gmail.com>
2016-05-11 14:21:39 -0700

    Enhanced JedisListener error messages

--- src/main/java/fogetti/phish/storm/db/JedisListener.java
+++ src/main/java/fogetti/phish/storm/db/JedisListener.java
@@ -8,6 +8,8 @@ import org.apache.storm.redis.common.container.JedisCommandsInstanceContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import redis.clients.jedis.Jedis;
 
 public class JedisListener implements Runnable {
@@ -17,6 +19,7 @@ public class JedisListener implements Runnable {
 	private JedisCallback callback;
 	private String channel;
 	private JedisCommandsInstanceContainer container;
+    private ObjectMapper mapper = new ObjectMapper();
 	
 	public JedisListener(JedisPoolConfig config, String channel, JedisCallback callback) {
 		this.channel = channel;
@@ -30,8 +33,14 @@ public class JedisListener implements Runnable {
 		while (!Thread.currentThread().isInterrupted()) {
 			try (Jedis jedis = (Jedis) container.getInstance()) {
 				List<String> messages = jedis.blpop(0, new String[]{channel});
-				for (String msg : messages) {
-			        callback.onMessage(channel, msg);
+				try {
+				    String msgs = mapper.writeValueAsString(messages);
+				    logger.info("Jedis message arrived {}", msgs);
+	                for (String msg : messages) {
+	                    callback.onMessage(channel, msg);
+	                }
+                } catch (Exception e) {
+                    logger.error("Sending intersection callback failed", e);
                 }
 			} catch (Exception e) {
 				logger.error("Subscribing to Redis failed - {}", e.getMessage());
