b3951c6d46d12e00d03e2a2664ddc4c74a653186
Fogetti <fogetti@gmail.com>
2016-05-16 15:36:19 -0700

    All topologies are reduced to 1

--- src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
+++ src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
@@ -50,17 +50,17 @@ public class PhishTopologyBuilder {
 		builder.setBolt("urlsplit", new URLBolt(), 1)
 			.fieldsGrouping("urlmatch", new Fields("word", "url"))
 			.setNumTasks(1);
-		builder.setBolt("googletrends-fast", new GoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 16)
+		builder.setBolt("googletrends-fast", new GoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 1)
 		    .addConfiguration("timeout", 5000)
 			.shuffleGrouping("urlsplit")
-			.setNumTasks(32);
-        builder.setBolt("googletrends-slow", new GoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 32)
+			.setNumTasks(1);
+        builder.setBolt("googletrends-slow", new GoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 1)
             .addConfiguration("timeout", 15000)
             .shuffleGrouping("googletrends-fast",
                     GoogleSemBolt.RETRY_STREAM)
             .shuffleGrouping("googletrends-slow",
                     GoogleSemBolt.RETRY_STREAM)
-            .setNumTasks(64);
+            .setNumTasks(1);
 		builder.setBolt("intersection", intersectionBolt(poolConfig, resultDataFile))
 			.globalGrouping("googletrends-fast",
 			        GoogleSemBolt.SUCCESS_STREAM)
