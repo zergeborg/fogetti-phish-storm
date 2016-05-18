175dc702bcd8e03afdb854910d27d360bb218454
Fogetti <fogetti@gmail.com>
2016-05-17 17:07:03 -0700

    JedisListener blocks for 60 seconds only and loops forever. Message timeout increased to 90 seconds

--- src/main/java/fogetti/phish/storm/db/JedisListener.java
+++ src/main/java/fogetti/phish/storm/db/JedisListener.java
@@ -8,18 +8,15 @@ import org.apache.storm.redis.common.container.JedisCommandsInstanceContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import redis.clients.jedis.Jedis;
 
 public class JedisListener implements Runnable {
 
 	private static final Logger logger = LoggerFactory.getLogger(JedisListener.class);
 
-	private JedisCallback callback;
-	private String channel;
-	private JedisCommandsInstanceContainer container;
-    private ObjectMapper mapper = new ObjectMapper();
+	private final JedisCallback callback;
+	private final String channel;
+	private final JedisCommandsInstanceContainer container;
 	
 	public JedisListener(JedisPoolConfig config, String channel, JedisCallback callback) {
 		this.channel = channel;
@@ -32,7 +29,7 @@ public class JedisListener implements Runnable {
 	    logger.info("Subscribing");
 		while (!Thread.currentThread().isInterrupted()) {
 			try (Jedis jedis = (Jedis) container.getInstance()) {
-				List<String> messages = jedis.blpop(0, new String[]{channel});
+				List<String> messages = jedis.blpop(60, new String[]{channel});
 				logger.info("Jedis message arrived {}", messages.get(1));
 				try {
                     callback.onMessage(channel, messages.get(1));
