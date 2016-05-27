a98f5f0199dcfdb41703f7280f8d83a75bd09f7a
Fogetti <fogetti@gmail.com>
2016-05-26 18:57:12 -0700

    Decreased the buffer size. Increased GoogleSemBolt parallelism

--- src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
+++ src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
@@ -46,10 +46,10 @@ public class PhishTopologyBuilder {
         builder.setBolt("urlmatch", new MatcherBolt(countDataFile, psDataFile, poolConfig), 1)
             .fieldsGrouping("urlsource", new Fields("url"))
             .setNumTasks(1);
-		builder.setBolt("googletrends", new ClientBuildingGoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 512)
+		builder.setBolt("googletrends", new ClientBuildingGoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 1024)
 		    .addConfiguration("timeout", 30000)
 		    .fieldsGrouping("urlmatch", new Fields("word", "url"))
-			.setNumTasks(512);
+			.setNumTasks(1024);
 		builder.setBolt("intersection", intersectionBolt(poolConfig, resultDataFile))
 			.globalGrouping("googletrends");
 		StormTopology topology = builder.createTopology();
