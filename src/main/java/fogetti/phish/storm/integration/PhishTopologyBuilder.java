8b07aba55393edd9fe506d708b8ef619e6172dc3
Fogetti <fogetti@gmail.com>
2016-06-03 15:35:06 -0700

    Experiment

--- src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
+++ src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
@@ -42,14 +42,14 @@ public class PhishTopologyBuilder {
 	        .setHost(redishost).setPort(redisport).setPassword(redispword).build();
 		builder
 			.setSpout("urlsource", new AsynchronousURLSpout(urlDataFile, poolConfig), 1)
-			.setMaxSpoutPending(50);
+			.setMaxSpoutPending(200);
         builder.setBolt("urlmatch", new MatcherBolt(countDataFile, psDataFile, poolConfig), 1)
             .fieldsGrouping("urlsource", new Fields("url"))
             .setNumTasks(1);
-		builder.setBolt("googletrends", new ClientBuildingGoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 768)
+		builder.setBolt("googletrends", new ClientBuildingGoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 192)
 		    .addConfiguration("timeout", 60000)
 		    .fieldsGrouping("urlmatch", new Fields("word", "url"))
-			.setNumTasks(768);
+			.setNumTasks(4608);
 		builder.setBolt("intersection", intersectionBolt(poolConfig, resultDataFile))
 			.globalGrouping("googletrends");
 		StormTopology topology = builder.createTopology();
