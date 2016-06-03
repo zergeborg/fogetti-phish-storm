7fe77c19327a049a6952a0e1ce43c7713557aac2
Fogetti <fogetti@gmail.com>
2016-06-02 22:51:58 -0700

    Changed parallelism

--- src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
+++ src/main/java/fogetti/phish/storm/integration/PhishTopologyBuilder.java
@@ -46,10 +46,10 @@ public class PhishTopologyBuilder {
         builder.setBolt("urlmatch", new MatcherBolt(countDataFile, psDataFile, poolConfig), 1)
             .fieldsGrouping("urlsource", new Fields("url"))
             .setNumTasks(1);
-		builder.setBolt("googletrends", new ClientBuildingGoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 192)
+		builder.setBolt("googletrends", new ClientBuildingGoogleSemBolt(poolConfig, new File(proxyDataFile), new WrappedRequest()), 384)
 		    .addConfiguration("timeout", 60000)
 		    .fieldsGrouping("urlmatch", new Fields("word", "url"))
-			.setNumTasks(9216);
+			.setNumTasks(4608);
 		builder.setBolt("intersection", intersectionBolt(poolConfig, resultDataFile))
 			.globalGrouping("googletrends");
 		StormTopology topology = builder.createTopology();
