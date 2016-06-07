6821e177f88fdbb0c9ef3d9778ffc3dd38f0da39
Fogetti <fogetti@gmail.com>
2016-06-06 20:20:59 -0700

    Increased buffer size

--- src/main/java/fogetti/phish/storm/integration/PhishTopologyRemoteRunner.java
+++ src/main/java/fogetti/phish/storm/integration/PhishTopologyRemoteRunner.java
@@ -14,11 +14,11 @@ public class PhishTopologyRemoteRunner {
 		config.setNumWorkers(60);
 		config.setMessageTimeoutSecs(180);
 		config.put(Config.TOPOLOGY_EXECUTOR_RECEIVE_BUFFER_SIZE,
-	               new Integer(32768));
+	               new Integer(1048576));
 	    config.put(Config.TOPOLOGY_EXECUTOR_SEND_BUFFER_SIZE,
-	               new Integer(32768));
+	               new Integer(1048576));
 	    config.put(Config.TOPOLOGY_TRANSFER_BUFFER_SIZE,
-	               new Integer(32768));
+	               new Integer(1048576));
 	    config.registerMetricsConsumer(LoggingMetricsConsumer.class, 1);
 		
 		StormSubmitter.submitTopology("phish-storm-topology", config, topology);
